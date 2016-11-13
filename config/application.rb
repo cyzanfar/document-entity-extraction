e04990b8f78073e49a56a0bc6c7dc7139bf04995
cyzanfar <cghazanfar10@gmail.com>
2016-11-13 12:28:25 -0500

    add env db route

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module DocumentReader
 
     config.neo4j.session_options = { basic_auth: { username: 'neo4j', password: ENV['NEO4J_PASSWORD']}, initialize: { ssl: { verify: true } } }
     config.neo4j.session_type = :server_db
-    config.neo4j.session_path = 'http://localhost:7474'
+    config.neo4j.session_path = ENV['GRAPHENEDB_URL'] || 'http://localhost:7474'
   end
 end
